69c88bb685ceb78a7ca4e6d6d88cf9602437d1f6
forex-inf <151822399+forex-inf@users.noreply.github.com>
2023-11-23 11:05:03 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+## For use installing lib  python-gitlab v4.1.1
+
+    pip install python-gitlab
+
+## For use set variable in blk_users.py
+
+    HOST - ip or domin gitlab server`s (string)
+    TOKEN - auth private token gitlab user (string)
---
+++ blk_users.py
@@ -0,0 +1,17 @@
+import sys
+import gitlab
+
+HOST = '192.168.136.15'
+TOKEN = 'glpat-WZrGkg2o11VajcpsDzWP'
+
+gl = gitlab.Gitlab(url=f'http://{HOST}/', private_token=TOKEN)
+
+for row in gl.users.list():
+    if(not row.is_admin):
+        try:
+            gl.users.get(row.id).unblock()
+            print(f'User: {row.username} was blocked.\n')
+        except:
+            print(f'User: {row.username} was not blocked, error.\n')
+    else:
+        print(f'User: {row.username} was not blocked, he is admin.\n')
